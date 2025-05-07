4fbf895b1962b2ded51d2524ac0d2753dd58ba9a
Magzhan Ziyadabek <mziyadabek@gmail.com>
2025-05-08 00:17:13 +0500

    bela

--- components/Chat/index.vue
+++ components/Chat/index.vue
@@ -25,37 +25,23 @@ onUpdated(() => {
 
 <template>
   <div class="flex h-full w-full flex-col">
-    <!-- Messages Container -->
-    <div class="flex-1 overflow-y-auto px-4 py-6 space-y-4">
+    <div class="flex-1 overflow-y-auto px-4 py-6 space-y-4 pb-24">
       <div
         v-for="msg in messages"
         :key="msg.id"
         class="flex"
         :class="msg.author === 'user' ? 'justify-end' : 'justify-start'"
       >
-        <!-- User Message -->
         <div
-          v-if="msg.author === 'user'"
           class="max-w-[70%] px-4 py-3 text-sm bg-blue-500 text-white rounded-lg rounded-br-none"
+          :class="
+            msg.author === 'user'
+              ? 'bg-blue-500 text-white rounded-br-none'
+              : 'bg-gray-100 text-gray-800 rounded-bl-none'
+          "
         >
           <p class="whitespace-pre-line">{{ msg.text }}</p>
           <p class="mt-1 text-[11px] text-blue-100 text-right">
-            {{
-              new Date(msg.time).toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-              })
-            }}
-          </p>
-        </div>
-
-        <!-- Assistant Message -->
-        <div
-          v-else
-          class="max-w-[70%] px-4 py-3 text-sm bg-gray-100 text-gray-800 rounded-lg rounded-bl-none"
-        >
-          <p class="whitespace-pre-line">{{ msg.text }}</p>
-          <p class="mt-1 text-[11px] text-gray-500 text-right">
             {{
               new Date(msg.time).toLocaleTimeString([], {
                 hour: "2-digit",
@@ -70,8 +56,10 @@ onUpdated(() => {
       <div ref="chatEndRef" class="h-4" />
     </div>
 
-    <!-- Input Container -->
-    <div class="mt-auto">
+    <!-- Input -->
+    <div
+      class="absolute bottom-0 left-0 right-0 bg-white border-t border-gray-200 p-4"
+    >
       <ChatInput @send="emit('send', $event)" />
     </div>
   </div>
